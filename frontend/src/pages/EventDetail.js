27d9103793bcaa8d99df6e83981e8f6e6af32d0f
Rajat Choudhary <rajatch133@gmail.com>
2023-09-03 15:29:16 +0530

    Task-6 solved route to eventsDetail page using id

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const EventDetail = () => {
   return (
-    <h1>EventDetail</h1>
+    <h1>The Event Details</h1>
   )
 }
 
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'Some event'
+  },
+  {
+    id: 'e2',
+    title: 'Another event'
+  },
+]
 const Events = () => {
   return (
-    <h1>Events</h1>
+    <>
+      <h1>Events</h1>
+      <ul>
+      {DUMMY_EVENTS.map((event)=> (
+        // relative path
+        <li key={event.id}><Link to={event.id}>{event.title}</Link></li>
+      ))}
+      </ul>
+    </>
   )
 }
 
