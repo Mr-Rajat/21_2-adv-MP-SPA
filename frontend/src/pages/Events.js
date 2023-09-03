abeb7fd2eca8ec1369efc9317e66cd61ef7e5ec0
Rajat Choudhary <rajatch133@gmail.com>
2023-09-03 16:29:57 +0530

    Reverted Back to Event.js for useLoader()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,14 +1,14 @@
-// import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 // This is special hook we can execute to get access to the closest loader data
 import EventsList from '../components/EventsList';
 
 const Events = () => {
-  // const events = useLoaderData();
+  const events = useLoaderData();
   
   return (
     <>
-      <EventsList />
-      {/* <EventsList events={events} /> */}
+      {/* <EventsList /> */}
+      <EventsList events={events} />
     </>
   );
 }
