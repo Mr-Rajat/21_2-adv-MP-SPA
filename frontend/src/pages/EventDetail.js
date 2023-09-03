e68ad942753acd46cfd96aa269948bae0485fa69
Rajat Choudhary <rajatch133@gmail.com>
2023-09-03 15:34:53 +0530

    TASK 7 done : displayed id passed in params

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const EventDetail = () => {
+  const params = useParams();
   return (
-    <h1>The Event Details</h1>
+    <>
+      <h1>The Event Details</h1>
+      {/* here the params.value should be same as defined in route */}
+      <p>{params.eventId}</p>
+    </>
   )
 }
 
