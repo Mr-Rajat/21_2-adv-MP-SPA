b832b006f6edd6b0735162a7589fe2125598274f
Rajat Choudhary <rajatch133@gmail.com>
2023-09-05 00:08:07 +0530

    created loader() for fetching event data in EvntDt

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { json, useParams } from 'react-router-dom'
+import EventItem from '../components/EventItem';
 
 const EventDetail = () => {
   const params = useParams();
+
   return (
     <>
-      <h1>The Event Details</h1>
-      {/* here the params.value should be same as defined in route */}
-      <p>{params.eventId}</p>
+      <EventItem event={}/>
+      
     </>
   )
 }
 
 export default EventDetail
+
+export const loader = async({request, params}) => {
+  // loader also get access to route value as params. as it cannot use any hook inside it. Loader accept 2 parameters
+  const id = params.eventId;
+  const response = await fetch('http://localhost:8080/events/'+ id);
+  if(!response.ok){
+    throw json({message: 'Could not fetch details for selected event.'}, {
+      status : 500,
+    })
+  } else {
+    return response;
+  }
+}
