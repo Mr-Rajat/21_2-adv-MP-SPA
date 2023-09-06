01f8e63ba7b468d0686065be24144fba93018c0a
Rajat Choudhary <rajatch133@gmail.com>
2023-09-07 00:05:31 +0530

    checked useFetcher() data change with inbuilt methods

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,9 +1,18 @@
 import { useFetcher } from 'react-router-dom';
 import classes from './NewsletterSignup.module.css';
+import { useEffect } from 'react';
 
 function NewsletterSignup() {
     // fetcher should be use whenver we want to trigger an action or also a loader with loader() without actually navigating to the page to which to loader belong or the page to which the action belong (fetcher provides diff methods like Form, Submit etc)
     const fetcher = useFetcher();
+    const { data, state } = fetcher;
+
+    useEffect(()=> {
+        if (state === 'idle' && data && data.message) {
+            window.alert(data.message);
+        }
+    }, [data, state]);
+
     // fetcher.
   return (
     <fetcher.Form method="post" action='/newsletter' className={classes.newsletter}>
