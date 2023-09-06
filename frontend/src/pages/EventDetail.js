d3126b5f9c43ad8f150d9401a71793f4f098d62e
Rajat Choudhary <rajatch133@gmail.com>
2023-09-07 01:20:42 +0530

    controlling defer data for single event and multiple events

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,32 +1,69 @@
-import React from 'react'
-import { json, redirect, useParams, useRouteLoaderData } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import { Await, defer, json, redirect, useParams, useRouteLoaderData } from 'react-router-dom'
 import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
 
 const EventDetail = () => {
   const params = useParams();
-  const data = useRouteLoaderData('event-detail');
+  const { event, events } = useRouteLoaderData('event-detail');
   // console.log(data);
   return (
     <>
-      <EventItem event={data.event} />
+    <Suspense fallback={ <p style={{textAlign: 'center'}}>Loading...</p>}>
+    <Await resolve={event}>
+      {loadedEvent => <EventItem event={loadedEvent} />}
+    </Await>
+    </Suspense>
+    
+    <Suspense fallback={ <p style={{textAlign: 'center'}}>Loading...</p>}>
+    <Await resolve={events}>
+      {loadedEvents => <EventsList events={loadedEvents}/> }
+    </Await>
+    </Suspense>
+      
+      
 
     </>
   )
 }
 
-export default EventDetail
+export default EventDetail;
 
-export const loader = async ({ request, params }) => {
-  // loader also get access to route value as params. as it cannot use any hook inside it. Loader accept 2 parameters
-  const id = params.eventId;
+const loadEvent= async(id) => {
+  
   const response = await fetch('http://localhost:8080/events/' + id);
   if (!response.ok) {
     throw json({ message: 'Could not fetch details for selected event.' }, {
       status: 500,
     })
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+
+}
+
+const loadEvents = async () => {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    throw json({ message: 'Could not fetch events.' }, {
+      status: 500,
+    });
+  } else {
+    const resData = await response.json();
+    return resData.events
+  }
+}
+
+export const loader = async ({ request, params }) => {
+  // loader also get access to route value as params. as it cannot use any hook inside it. Loader accept 2 parameters
+  const id = params.eventId;
+  
+  return defer({
+    event: loadEvent(id),
+    events: loadEvents(),
+  })
 }
 
 export const action = async ({ params, request }) => {
